3339e8b0b649bb61e2bad5bce1a52c381211251b
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-06-18 06:22:37 -0700

    Add example_instructions.py from codellama

--- example_instructions.py
+++ example_instructions.py
@@ -57,14 +57,11 @@ def main(
 
     for instruction, result in zip(instructions, results):
         for msg in instruction:
-            print(f"{msg['role'].capitalize()}: {msg['content']}
-")
+            print(f"{msg["role"].capitalize()}: {msg["content"]}\n")
         print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+            f"> {result["generation"]["role"].capitalize()}: {result["generation"]["content"]}"
         )
-        print("
-==================================
-")
+        print("\n==================================\n")
 
 
 if __name__ == "__main__":
