1a82f877d5a450af4a788c45617a895eaa4229f5
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-08-14 17:33:41 -0700

    Add example instructions

--- example_instructions.py
+++ example_instructions.py
@@ -57,12 +57,16 @@ def main(
 
     for instruction, result in zip(instructions, results):
         for msg in instruction:
-            print(f"{msg["role"].capitalize()}: {msg["content"]}\n")
+            print(f"{msg["role"].capitalize()}: {msg["content"]}
+")
         print(
             f"> {result["generation"]["role"].capitalize()}: {result["generation"]["content"]}"
         )
-        print("\n==================================\n")
+        print("
+==================================
+")
 
 
 if __name__ == "__main__":
     fire.Fire(main)
+
