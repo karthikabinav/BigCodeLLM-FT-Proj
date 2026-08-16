1097383680e577ff99835d81b98d2b4fc302c327
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-08-15 19:02:47 -0700

    Add example instructions

--- example_instructions.py
+++ example_instructions.py
@@ -59,8 +59,7 @@ def main(
         for msg in instruction:
             print(f"{msg[role].capitalize()}: {msg[content]}\n")
         print(
-            f"> {result[generation][role].capitalize()}: {result[generation][content]}"
-        )
+            f"> {result[generation][role].capitalize()}: {result[generation][content]}"\n        )
         print("\n==================================\n")
 
 
