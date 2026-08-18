bac8a441a993b4fdde4c7511493ccef02548419d
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-08-18 03:03:42 -0700

    Add example instructions

--- example_instructions.py
+++ example_instructions.py
@@ -57,11 +57,11 @@ def main(
 
     for instruction, result in zip(instructions, results):
         for msg in instruction:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+            print(f"{msg[role].capitalize()}: {msg[content]}\\n")
         print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+            f"> {result[generation][role].capitalize()}: {result[generation][content]}"
         )
-        print("\n==================================\n")
+        print("\\n==================================\\n")
 
 
 if __name__ == "__main__":
