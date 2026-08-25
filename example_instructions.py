2223110c1e0a80f72bb20fcdaf998640517cf52e
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-08-24 20:09:47 -0700

    Add placeholder for example_instructions with license guidance

---
+++ example_instructions.py
@@ -0,0 +1,8 @@
+# Placeholder for example_instructions.py
+# The original file from meta-llama/codellama is subject to the Llama 2 Community License Agreement.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# To comply with licensing terms, the full file is not reproduced here.
+# Please refer to the original source:
+# https://github.com/meta-llama/codellama/blob/main/example_instructions.py
+# Ensure you review and accept the Llama 2 Community License before use:
+# https://github.com/facebookresearch/llama/blob/main/LICENSE
